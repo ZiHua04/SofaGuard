12b2e5ef2c2caa604382804ecdbaf98a5446eea7
ZiHua04 <1227097374@qq.com>
2023-06-02 17:27:05 +0800

    改为mqtt协议，并添加了测试模块

--- pages/index/contorl/contorl.js
+++ pages/index/contorl/contorl.js
@@ -1,21 +1,60 @@
 // pages/index/contorl/contorl.js
+const app = getApp()
+import mqtt from '../../../utils/mqtt.min.js';//加载mqtt库
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    uid: '7e5233d024874b63a5929338ee676385',
-    topic: "test",
+    uid: '76c9ac8524f64e1887c6a7c6272bbb23',
+    topic: "esp",
     device_status: "离线", //默认离线
-    powerstatus:"已关闭"   //默认关闭
-  },
-  data: {
+    powerstatus:"已关闭" ,  //默认关闭
     imageRightSrc:"../../../image/arrowright.png",
     imageLeftSrc:"../../../image/arrowleft.png",
-    uid:"7e5233d024874b63a5929338ee676385",
-    topic:"test"
+    testled:false,
+    testmas:"这段显示主题中的消息"
+
   },
+
+  mqttConnect(){
+    var that = this;
+    var options= {
+      keepalive: 60, //60s ，表示心跳间隔
+      clean: true, //cleanSession不保持持久会话
+      protocolVersion: 4, //MQTT v3.1.1
+      clientId:this.data.uid
+    }
+    //初始化mqtt连接
+    this.data.client = mqtt.connect('wxs://bemfa.com:9504/wss',options);
+    // 连接mqtt服务器
+    this.data.client.on('connect', function () {
+      console.log('连接服务器成功')
+      //订阅dht11温湿度主题
+      that.data.client.subscribe(that.data.topic, function (err) {
+        if (err) {
+            console.log(err)
+        }
+      })
+      
+    })
+    //接收消息
+    that.data.client.on('message', function (topic, message) {
+      console.log(topic)
+      var  msg = message.toString()
+      if(topic == that.data.topic){//如果是**主题的消息
+        that.setData({testmas:msg})
+      }
+      //打印消息
+      console.log('收到消息：'+msg)
+    })
+    //断线重连
+    this.data.client.on("reconnect", function () {
+      console.log("重新连接")
+    });
+  },
+
   rightTouch:function(){
     this.setData({
       imageRightSrc:"../../../image/_arrowright.png"
@@ -25,45 +64,13 @@ Page({
         console.log('震动反馈成功')
       }
     })
-    var that = this;
-    wx.request({
-      url: 'https://api.bemfa.com/api/device/v1/data/1/',
-      method:"POST",
-      data:{
-        uid:that.data.uid,
-        topic:that.data.topic,
-        msg:"right"
-      },
-      header:{
-        'content-type': "application/x-www-form-urlencoded"
-      },
-      success(res){
-        console.log(res.data);
-        console.log("向右转成功")
-      }
-    })
+    this.data.client.publish(this.data.topic,'right');
   },
   rightTouchEnd:function(){
     this.setData({
       imageRightSrc:"../../../image/arrowright.png"
     })
-    var that = this;
-    wx.request({
-      url: 'https://api.bemfa.com/api/device/v1/data/1/',
-      method:"POST",
-      data:{
-        uid:that.data.uid,
-        topic:that.data.topic,
-        msg:"stop"
-      },
-      header:{
-        'content-type': "application/x-www-form-urlencoded"
-      },
-      success(res){
-        console.log(res.data);
-        console.log("停止成功")
-      }
-    })
+    this.data.client.publish(this.data.topic,"stop");
   },
   leftTouch:function(){
     this.setData({
@@ -74,85 +81,41 @@ Page({
         console.log('震动反馈成功')
       }
     });
-    var that = this;
-    wx.request({
-      url: 'https://api.bemfa.com/api/device/v1/data/1/',
-      method:"POST",
-      data:{
-        uid:that.data.uid,
-        topic:that.data.topic,
-        msg:"left"
-      },
-      header:{
-        'content-type': "application/x-www-form-urlencoded"
-      },
-      success(res){
-        console.log(res.data);
-        console.log("向左转成功")
-      }
-    })
+    this.data.client.publish(this.data.topic,"left");
   },
   leftTouchEnd:function(){
     this.setData({
       imageLeftSrc:"../../../image/arrowleft.png"
     })
-    var that = this;
-    wx.request({
-      url: 'https://api.bemfa.com/api/device/v1/data/1/',
-      method:"POST",
-      data:{
-        uid:that.data.uid,
-        topic:that.data.topic,
-        msg:"stop"
-      },
-      header:{
-        'content-type': "application/x-www-form-urlencoded"
-      },
-      success(res){
-        console.log(res.data);
-        console.log("停止成功")
-      }
-    })
+    this.data.client.publish(this.data.topic,"stop");
   },
-  button1:function()
-  {
-    
-      that.setData({
-        powerstatus:"已打开"
-      })
-      wx.request({
-        url: 'https://api.bemfa.com/api/device/v1/data/1/', //api接口，详见接入文档
-        method:"POST",
-        data: {  //请求字段，详见巴法云接入文档，http接口
-          uid: that.data.uid,
-          topic: that.data.topic,
-          msg:"on"   //发送消息为on的消息
-        },
-        header: {
-          'content-type': "application/x-www-form-urlencoded"
-        },
-        success (res) {
-          console.log(res.data)
-          wx.showToast({
-            title:'打开成功',
-            icon:'success',
-            duration:1000
-          })
-        }
-      })
+  teston:function(){
+    this.data.client.publish(this.data.topic,"on");
+  },
+  testoff:function(){
+    this.data.client.publish(this.data.topic,"off");
   },
   
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function () {
+  onLoad(){
+    //连接mqtt
+    this.mqttConnect()
+    //检查设备是否在线
+    this.getOnline()
+    //检查设备是打开还是关闭
+    this.getOnOff()
+    //获取服务器上现在存储的dht11数据
+    //this.getdht11()
+  },
+  getOnline(){
     var that = this
-
-    //请求设备状态
-    //设备断开不会立即显示离线，由于网络情况的复杂性，离线1分钟左右才判断真离线
+    //请求设备状态,检查设备是否在线
+     //api 接口详细说明见巴法云接入文档
     wx.request({
-      url: 'https://api.bemfa.com/api/device/v1/status/', //状态api接口，详见巴法云接入文档
+      url: 'https://api.bemfa.com/mqtt/status/', //状态api接口，详见巴法云接入文档
       data: {
         uid: that.data.uid,
         topic: that.data.topic,
@@ -173,79 +136,43 @@ Page({
         }
         console.log(that.data.device_status)
       }
-    })
-
-          //请求询问设备开关/状态
-          wx.request({
-            url: 'https://api.bemfa.com/api/device/v1/data/1/', //get接口，详见巴法云接入文档
-            data: {
-              uid: that.data.uid,
-              topic: that.data.topic,
-            },
-            header: {
-              'content-type': "application/x-www-form-urlencoded"
-            },
-            success (res) {
-              console.log(res.data)
-              if(res.data.msg === "on"){
-                that.setData({
-                  powerstatus:"已打开"
-                })
-              }
-              console.log(that.data.powerstatus)
-            }
-          })
-
-
-    //设置定时器，每五秒请求一下设备状态
-    setInterval(function () {
-      console.log("定时请求设备状态,默认五秒");
-      wx.request({
-        url: 'https://api.bemfa.com/api/device/v1/status/',  //get 设备状态接口，详见巴法云接入文档
-        data: {
-          uid: that.data.uid,
-          topic: that.data.topic,
-        },
-        header: {
-          'content-type': "application/x-www-form-urlencoded"
-        },
-        success (res) {
-          console.log(res.data)
-          if(res.data.status === "online"){
+    })    
+  },
+  getOnOff(){
+    //获取设备状态，检查设备是打开还是关闭
+    //api 接口详细说明见巴法云接入文档
+    var that = this
+    wx.request({
+      url: 'https://api.bemfa.com/api/device/v1/data/3/get/', //状态api接口，详见巴法云接入文档
+      data: {
+        uid: that.data.uid,
+        topic: that.data.topic,
+        num:1
+      },
+      header: {
+        'content-type': "application/x-www-form-urlencoded"
+      },
+      success (res) {
+        console.log(res)
+        if("undefined" != typeof res.data.data){//判断是否获取到温湿度
+          console.log(res.data.data[0].msg)
+          if(res.data.data[0].msg == "on"){
             that.setData({
-              device_status:"在线"
+              checked:true,
+              ledOnOff:"打开",
+              ledicon: "/utils/img/lighton.png",
             })
           }else{
             that.setData({
-              device_status:"离线"
+              checked:false,
+              ledOnOff:"关闭",
+              ledicon: "/utils/img/lightoff.png",
             })
           }
-          console.log(that.data.device_status)
         }
-      })
 
-      //请求询问设备开关/状态
-      wx.request({
-        url: 'https://api.bemfa.com/api/device/v1/data/1/', //get接口，详见巴法云接入文档
-        data: {
-          uid: that.data.uid,
-          topic: that.data.topic,
-        },
-        header: {
-          'content-type': "application/x-www-form-urlencoded"
-        },
-        success (res) {
-          console.log(res.data)
-          if(res.data.msg === "on"){
-            that.setData({
-              powerstatus:"已打开"
-            })
-          }
-          console.log(that.data.powerstatus)
-        }
-      })
-
-    }, 5000)
+      }
+    })    
   },
 
   /**
